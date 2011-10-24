e0830e8831b6c394b9ca6c577986566e4c91b05e
mlopez@devgear.com.ar <mlopez@devgear.com.ar@0d231fd9-d98e-2959-c8da-8f0789fc113e>
2011-10-24 04:42:59 +0000

    - Application is working again - UserService & UserDao added

---
+++ trunk/codesizer/src/main/java/com/devgear/codesizer/core/dao/IUserDao.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.devgear.codesizer.core.dao;
+
+import com.devgear.codesizer.model.User;
+import com.googlecode.genericdao.dao.jpa.GenericDAO;
+
+/**
+ * @author mlopez
+ *
+ */
+public interface IUserDao extends GenericDAO<User, Integer> {
+
+}
---
+++ trunk/codesizer/src/main/java/com/devgear/codesizer/core/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.devgear.codesizer.core.service.impl;
+
+import com.devgear.codesizer.core.dao.IUserDao;
+import com.devgear.codesizer.core.service.IUserService;
+import com.devgear.codesizer.core.util.service.BaseService;
+import com.devgear.codesizer.model.User;
+
+/**
+ * @author mlopez
+ * 
+ */
+public class UserServiceImpl extends BaseService<User, Integer, IUserDao> implements IUserService {
+
+}
