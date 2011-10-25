1d48051240305a9186a60a89f44000aacb05c80d
mlopez@devgear.com.ar <mlopez@devgear.com.ar@0d231fd9-d98e-2959-c8da-8f0789fc113e>
2011-10-25 02:56:16 +0000

    - Now database is accessed through application server connection pool exposed via JNDI - Added support for integration JUnit tests (with h2 in memory database with automatic creation of schema)

--- trunk/codesizer/src/main/java/com/devgear/codesizer/core/service/impl/UserServiceImpl.java
+++ trunk/codesizer/src/main/java/com/devgear/codesizer/core/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@
  */
 package com.devgear.codesizer.core.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.devgear.codesizer.core.dao.IUserDao;
 import com.devgear.codesizer.core.service.IUserService;
 import com.devgear.codesizer.core.util.service.BaseService;
@@ -12,6 +14,7 @@ import com.devgear.codesizer.model.User;
  * @author mlopez
  * 
  */
+@Service("UserService")
 public class UserServiceImpl extends BaseService<User, Integer, IUserDao> implements IUserService {
-
+		
 }
